bd3d6f01a55fb9a65ae6040f65b7ae59e5cd57b4
Sathish <sathishms0305@gmail.com>
2022-09-14 13:26:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNav from './components/Navbar';
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div>
       <TopNav/>
+      <Hero/>
     </div>
   );
 }
