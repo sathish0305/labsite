86c391a0979d15ca0b4ce82351844a8f7ea3bf1e
Sathish <sathishms0305@gmail.com>
2022-09-19 11:59:57 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import '../css/Home.css'
-import { useState, useEffect } from 'react'; 
+
 
 function Home() {
-  const { innerWidth: width, innerHeight: height } = window;
   return (
     <div className='hero'>
         <div>
