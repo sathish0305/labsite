3bf8f8e2977b672d18d11c5188d1a0d172a8659f
Sathish <sathishms0305@gmail.com>
2022-09-20 14:47:27 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Slider from '../components/slider/Slider';
+import Services from '../pages/Services'
+import About from '../pages/About'
+import Features from '../pages/Features'
+import Contact from '../pages/Contact'
 
 function Home() {
   return (
    <>
    <Slider/>
+   <Services/>
+   <About/>
+   <Features/>
+   <Contact/>
    </>
   )
 }
