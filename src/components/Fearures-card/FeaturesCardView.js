86d80cc7a26cc0b0aa05c0902b0909c255e0d7c9
Sathish <sathishms0305@gmail.com>
2022-09-23 12:58:13 +0530

    features commit

--- src/components/Fearures-card/FeaturesCardView.js
+++ src/components/Fearures-card/FeaturesCardView.js
@@ -11,33 +11,34 @@ function FeaturesCardView() {
   </div>
   <div class="f-cards">
     <div class="f-card f-card-1">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    
+      <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
     </div>
     <div class="f-card f-card-2">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
     </div>
     <div class="f-card f-card-3">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Ut enim ad minim veniam.</h2>
     </div>
     <div class="f-card f-card-4">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Quis nostrud exercitation ullamco laboris nisi.</h2>
     </div>
     <div class="f-card f-card-5">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Ut aliquip ex ea commodo consequat. Duis aute irure dolor.</h2>
     </div>
     <div class="f-card f-card-1">
-      <div class="f-card__icon"><i class="fas fa-bolt"></i></div>
-      <h1><span className='f-card__link'>Feature Title</span></h1>
+    <h1 className='head'><span className='f-card__link'>Feature Title</span></h1>
+      <span className='icon'><i class="fas fa-bolt"></i></span>
       <h2 class="f-card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
     </div>
   </div>
